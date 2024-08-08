a3ce117a1eb77f669446bd57ec60f549960e6843
Tahmid-kabir-addin <tahmidkabiraddin@gmail.com>
2024-08-08 19:15:53 +0600

    cronjob initiated

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -1,14 +1,87 @@
+const CronJob = require("cron").CronJob;
 const fs = require("fs");
 const path = require("path");
 const pm2 = require("pm2");
-const { fileURLToPath } = require("url");
-// Emulate __dirname
+
+const schedule = {
+  startDate: "08/08/2024",
+  startTime: "16:40",
+  set_end_date: false,
+  endDate: null,
+  endTime: null,
+};
+const title = "test3";
 
 // Define the log file path
 const logFilePath = path.join(
-  path.dirname(__filename),
-  "scheduleLog.txt"
+  __dirname,
+  "../cronLogs",
+  `${title}-SaleCronLog.txt`
 );
 const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
 
-logStream.write(`Scheduler started at ${new Date().toISOString()}\n`);
+logStream.write(`Starting the cron job....${title}\n`);
+
+const { startDate, startTime, set_end_date, endDate, endTime } = schedule;
+const [startDay, startMonth, startYear] = startDate.split("/").map(Number);
+const [startHour, startMinute] = startTime.split(":").map(Number);
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1; // Months are zero based
+const currentDay = currentDate.getDate();
+const currentHours = currentDate.getHours();
+const currentMinutes = currentDate.getMinutes();
+
+if (
+  currentYear >= parseInt(startYear) &&
+  currentMonth >= parseInt(startMonth) &&
+  currentDay >= parseInt(startDay) &&
+  currentHours >= parseInt(startHour) &&
+  currentMinutes >= parseInt(startMinute)
+) {
+  cronFunction();
+}
+
+const cronExpression = `${startMinute} ${startHour} ${startDay} ${startMonth} *`;
+// const cronExpression = "* * * * *";
+logStream.write(`Cron expression: ${cronExpression}\n`);
+const cronJob = new CronJob(cronExpression, cronFunction);
+
+function yearFilter(year) {
+  return parseInt(startYear) === year;
+}
+
+function cronFunction() {
+  if (!yearFilter(new Date().getFullYear())) {
+    return;
+  }
+  try {
+    logStream.write("cronFunction: Running....\n");
+    // throw new Error("Something went wrong in the cron job");
+  } catch (error) {
+    logStream.write(`cronFunction: Error: ${error.message}\n`);
+    stopPm2Instance();
+  }
+}
+
+function stopPm2Instance() {
+  pm2.connect((err) => {
+    if (err) {
+      logStream.write(
+        `Error connecting to pm2 to stop instance: ${err.message}\n`
+      );
+      return;
+    }
+    pm2.stop(`scheduler`, (err) => {
+      if (err) {
+        logStream.write(`Error stopping pm2 instance: ${err.message}\n`);
+      } else {
+        logStream.write(`Pm2 instance stopped successfully.\n`);
+      }
+      pm2.disconnect();
+    });
+  });
+}
+
+cronJob.start();
--- utils/startInstance.js
+++ utils/startInstance.js
@@ -1,30 +1,66 @@
 const pm2 = require("pm2");
+const path = require("path");
+const fs = require("fs");
 
-const scriptPath = "./scheduler.js"; // Adjust the path to your script
+const runPm2 = (title) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const scriptPath = path.join(__dirname, "scheduler.js"); // Adjust the path to your script
 
-const runPm2 = () => {
-  console.log("Starting PM2 instance...");
-  pm2.connect((err) => {
-    if (err) {
-      console.error(err);
-      process.exit(2);
+      // Define the log file path
+      const logFilePath = path.join(
+        __dirname,
+        "../instanceLogs",
+        `${title}-instance.txt`
+      );
+      const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
+
+      logStream.write("Starting the instance....\n");
+
+      pm2.connect((err) => {
+        if (err) {
+          logStream.write(`Error connecting to pm2:\n ${err.message}`);
+          console.log(`Error connecting to pm2:\n ${err.message}`);
+          // todo: send an email to the user if any error occurred
+          reject({ status: "error", message: err.message });
+          return;
+        }
+        pm2.start(
+          {
+            script: scriptPath, // Script to be run
+            name: `${title}-sale instance`, // Name of the process
+            instances: 1, // Number of instances
+          },
+          (err, apps) => {
+            if (err) {
+              logStream.write(
+                `Error starting the pm2 instance:\n ${err.message}`
+              );
+              console.log(`Error starting the pm2 instance:\n ${err.message}`);
+              // todo: send an email to the user if any error occurred
+              reject({ status: "error", message: err.message });
+              return;
+            }
+            pm2.disconnect(); // Disconnects from PM2
+            logStream.write("Pm2 instance started...\n");
+            resolve({ status: "success", message: "Pm2 instance started" });
+          }
+        );
+      });
+    } catch (error) {
+      console.log(`Exception in runPm2:\n ${error.message}`);
+      logStream.write(`Exception in runPm2:\n ${error.message}`);
+      // todo: send an email to the user if any error occurred
+      reject({ status: "error", message: error.message });
     }
-
-    pm2.start(
-      {
-        script: scriptPath, // Script to be run
-        name: "scheduler-instance", // Name of the process
-        instances: 1, // Number of instances
-      },
-      (err, apps) => {
-        pm2.disconnect(); // Disconnects from PM2
-        if (err) throw err;
-        console.log("PM2 instance started:", apps);
-      }
-    );
   });
 };
-
-module.exports = runPm2;
-
-// export default runPm2;
+(async () => {
+  try {
+    const state = await runPm2("test2");
+    console.log(state);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+// module.exports = runPm2;
